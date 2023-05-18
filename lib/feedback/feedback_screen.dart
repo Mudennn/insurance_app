c14a21b20acefde8e722fe78d032d686c1a12316
Muden <mudennn@yahoo.com>
2023-05-18 11:52:55 +0800

    update feedback page

--- lib/feedback/feedback_screen.dart
+++ lib/feedback/feedback_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../components/custome_appbar.dart';
+import '../responsive.dart';
 import 'components/body.dart';
+import 'components/desktop_body.dart';
 
 class FeedbackScreen extends StatelessWidget {
   static String routeName = "/feedback";
@@ -8,14 +10,14 @@ class FeedbackScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(
+    return  Scaffold(
+      appBar: const CustomAppBar(
         title: "Feedback",
         // leading: Text("Feedback", style: TextStyle(
         //       fontWeight: FontWeight.bold, fontSize: 22, color: Colors.white) ),
         
       ),
-      body: Body(),
+      body: !Responsive.isDesktop(context) ? const Body() : const DesktopBody(),
     );
   }
 }
